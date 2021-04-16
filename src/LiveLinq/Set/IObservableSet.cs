162efd381d65d9d0295b613a5342a61cd3cc7aa9
Nate <39776302+nloum@users.noreply.github.com>
2021-04-16 09:21:03 -0500

    Add documentation

--- src/LiveLinq/Set/IObservableSet.cs
+++ src/LiveLinq/Set/IObservableSet.cs
@@ -2,6 +2,10 @@ using ComposableCollections.Set;
 
 namespace LiveLinq.Set
 {
+    /// <summary>
+    /// A set data structure that provides callbacks for when it changes.
+    /// </summary>
+    /// <typeparam name="TValue">The type of item in the set</typeparam>
     public interface IObservableSet<TValue> : IComposableSet<TValue>, IObservableReadOnlySet<TValue>
     {
         
