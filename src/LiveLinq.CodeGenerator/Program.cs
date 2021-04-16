fd442246e27182b3efd75d968803e7d2a4f33161
Nate <39776302+nloum@users.noreply.github.com>
2021-04-15 20:35:23 -0500

    remove refs to external code

--- src/LiveLinq.CodeGenerator/Program.cs
+++ src/LiveLinq.CodeGenerator/Program.cs
@@ -12,23 +12,23 @@ namespace LiveLinq.CodeGenerator
     {
 	    static void Main(string[] args)
 	    {
-		    var ioService = new IoService(new ReactiveProcessFactory());
-		    var repoRoot = ioService.CurrentDirectory.Ancestors().First(ancestor => (ancestor / ".git").IsFolder());
-
-		    var solutionFile = repoRoot / "src" / "LiveLinq.sln";
-		    var liveLinqCsproj = repoRoot / "src" / "LiveLinq" / "LiveLinq.csproj";
-
-		    var codeIndexBuilder = new CodeIndexBuilder();
-		    codeIndexBuilder.AddProject(solutionFile.ToString(), liveLinqCsproj.ToString());
-
-		    var codeIndex = codeIndexBuilder.Build();
-
-		    var readOnlyComposableDictionary = codeIndex.GetValue(new TypeIdentifier
-		    {
-			    Namespace = "ComposableCollections.Dictionary.Interfaces",
-			    Name = "IReadOnlyComposableDictionary",
-			    Arity = 2,
-		    });
+		    // var ioService = new IoService(new ReactiveProcessFactory());
+		    // var repoRoot = ioService.CurrentDirectory.Ancestors().First(ancestor => (ancestor / ".git").IsFolder());
+		    //
+		    // var solutionFile = repoRoot / "src" / "LiveLinq.sln";
+		    // var liveLinqCsproj = repoRoot / "src" / "LiveLinq" / "LiveLinq.csproj";
+		    //
+		    // var codeIndexBuilder = new CodeIndexBuilder();
+		    // codeIndexBuilder.AddProject(solutionFile.ToString(), liveLinqCsproj.ToString());
+		    //
+		    // var codeIndex = codeIndexBuilder.Build();
+		    //
+		    // var readOnlyComposableDictionary = codeIndex.GetValue(new TypeIdentifier
+		    // {
+			   //  Namespace = "ComposableCollections.Dictionary.Interfaces",
+			   //  Name = "IReadOnlyComposableDictionary",
+			   //  Arity = 2,
+		    // });
 	    }
     }
 }
